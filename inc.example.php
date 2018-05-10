4e3b4ede68097a4a134008c5acf4f20a65b4cce6
drice82 <xujiabin82@gmail.com>
2018-05-10 22:20:12 +0800

    Update inc.example.php

--- inc.example.php
+++ inc.example.php
@@ -3,5 +3,7 @@
 $da_site = 'da.example.com';
 $da_admin = 'admin';
 $da_password = 'password';
+$da_ip = '91.123.124.1';
 $cf_email = 'your@gmail.com';
 $cf_key = 'api_key';
+$cf_domain = 'mydomain.com';
