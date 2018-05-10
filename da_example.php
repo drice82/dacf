423ccedb07a90702669dca9fba2a0192c5fa76cd
drice82 <xujiabin82@gmail.com>
2018-05-10 21:59:54 +0800

    Update da_example.php

--- da_example.php
+++ da_example.php
@@ -8,11 +8,33 @@ $sock = new HTTPSocket;
 $sock->connect($da_site,2222);
 $sock->set_login($da_admin,$da_password);
 
-$show_user='admin';
+//list users
+$sock->query('/CMD_API_SHOW_USERS');
+$users=$sock->fetch_parsed_body();
 
-$sock->query('/CMD_API_SHOW_USER_CONFIG?user='.$show_user);
-$result = $sock->fetch_parsed_body();
+foreach($users['list'] as $user) {
+    //get domains for user
+    $sock->query(
+        '/CMD_API_SHOW_USER_DOMAINS',
+        array(
+            'user'=>$user
+        )
+    );
 
-print_r($result);
+    $domains=$sock->fetch_parsed_body();
+  
+    $regex = '/.+(?=_team-disk_com)/';
+    $get_result = array_keys($domains)[0];
+    $sub_domain = array();
+
+    if (preg_match($regex, $get_result, $sub_domain)){
+        print_r($sub_domain[0]);
+	  }
+
+  
+}
+
+
+
 
 ?>
