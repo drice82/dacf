0d160cf1f8c9b2e303e53f37eb434467b20f902f
drice82 <xujiabin82@gmail.com>
2018-05-11 15:54:34 +0800

    Update del.php

--- del.php
+++ del.php
@@ -11,13 +11,42 @@ $zones = new \Cloudflare\API\Endpoints\Zones($adapter);
 $zoneID = $zones->getZoneID($cf_domain);
 $dns = new \Cloudflare\API\Endpoints\DNS($adapter);
 
+
+//get DA domain
+$sock = new HTTPSocket;
+$sock->connect($da_site,2222);
+$sock->set_login($da_admin,$da_password);
+
+//list users
+$sock->query('/CMD_API_SHOW_USERS');
+$users=$sock->fetch_parsed_body();
+
+foreach($users['list'] as $user) {
+    //get domains for user
+    $sock->query(
+        '/CMD_API_SHOW_USER_DOMAINS',
+        array(
+            'user'=>$user
+        )
+    );
+
+    $domains=$sock->fetch_parsed_body();
+    $regex = '/.+(?=_team-disk_com)/';
+    $get_result = array_keys($domains)[0];
+    $sub_domain = array();
+    if (preg_match($regex, $get_result, $sub_domain)){
+        $da_name_list[] = $sub_domain[0];
+    }
+}
+print_r($da_name_list);
+
 //$cf_name_list: get all the secondary domain name;
 $cf_name_list = array();
 foreach ($dns->listRecords($zoneID)->result as $record) {
 	$regex = '/.+(?=\.team-disk\.com)/';
  	$get_result = $record->name;
 	if (preg_match($regex, $get_result, $sub_domain)){
-	        $name_value = $sub_domain[0];
+	    $name_value = $sub_domain[0];
 		$cf_name_list[] = $name_value;
 	}
 }
